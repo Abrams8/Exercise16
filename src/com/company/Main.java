6028f9482422652f0020108962e99224fa6a8c2d
Abrams <dima.abramchik@gmail.com>
2021-05-20 00:14:03 +0300

    Init

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number = 1;
+        int del = 1;
+        for (; number <= 100; del = number % 7) {
+            if (del == 0) {
+                System.out.println("Число " + number + " делится на 7");
+                number++;
+            } else {
+                number++;
+            }
+        }
+    }
+}
