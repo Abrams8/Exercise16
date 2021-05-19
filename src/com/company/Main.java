962fd7937e39fa09c357ed920f3d9875d95faee9
Abrams <dima.abramchik@gmail.com>
2021-05-20 00:15:11 +0300

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int del = 1;
         for (; number <= 100; del = number % 7) {
             if (del == 0) {
-                System.out.println("Число " + number + " делится на 7");
+                System.out.println("Число " + number + " делится на 7!");
                 number++;
             } else {
                 number++;
